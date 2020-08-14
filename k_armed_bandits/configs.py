b50585eb2852baf1ea9973c3113653db60fe686c
Josiah-tan <65082231+Josiah-tan@users.noreply.github.com>
2020-08-15 01:39:57 +1000

    Update configs.py

--- k_armed_bandits/configs.py
+++ k_armed_bandits/configs.py
@@ -12,3 +12,5 @@
 k = 10 # number of actions to choose
 tru_val_variance, tru_val_mean= 1, 0  # variance and mean for initialisation of q_star(a)
 sample_val_variance = 1 # variance for sample action-val distribution for every action
+steps = 100 # number of steps
+runs = 100
