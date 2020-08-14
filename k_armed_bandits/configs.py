6982eeab6a3181603308fa48f32258ee7d12b882
Josiah-tan <65082231+Josiah-tan@users.noreply.github.com>
2020-08-14 21:29:46 +1000

    Update and rename package_imports.py to configs.py

---
+++ k_armed_bandits/configs.py
@@ -0,0 +1,14 @@
+#================================================================
+#
+#   File name   : configs.py
+#   Author      : Josiah Tan
+#   Created date: 10/07/2020
+#   Description : configuration file for k_armed_bandits
+#
+#================================================================
+
+#================================================================
+
+k = 10 # number of actions to choose
+
+action_val_variance = 1 # variance for action-val distribution for every action
