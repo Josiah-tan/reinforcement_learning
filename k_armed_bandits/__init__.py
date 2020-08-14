98a9a780741968fb62b00148a22552f3cc38bf5f
Josiah-tan <65082231+Josiah-tan@users.noreply.github.com>
2020-08-14 23:39:24 +1000

    Update __init__.py

--- k_armed_bandits/__init__.py
+++ k_armed_bandits/__init__.py
@@ -10,3 +10,4 @@
 #================================================================
 
 from .k_armed_bandits import * # this basically imports everything from k_armed_bandits.py so that you can use the stuff in k_armed_bandits.py as a module simply by using from k_armed_bandits import * 
+from .agent import *
