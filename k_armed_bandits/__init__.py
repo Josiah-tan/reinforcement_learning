4a92e5365024dc896cf5fb18aca6e4745e846377
Josiah-tan <65082231+Josiah-tan@users.noreply.github.com>
2020-08-14 20:47:20 +1000

    Create __init__.py

---
+++ k_armed_bandits/__init__.py
@@ -0,0 +1,12 @@
+#================================================================
+#
+#   File name   : __init__.py
+#   Author      : Josiah Tan
+#   Created date: 17/07/2020
+#   Description : __init__ file for k_armed_bandits
+#
+#================================================================
+
+#================================================================
+
+from .k_armed_bandits import * # this basically imports everything from k_armed_bandits.py so that you can use the stuff in k_armed_bandits.py as a module simply by using from k_armed_bandits import * 
