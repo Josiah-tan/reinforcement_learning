01a8b5ed66310ad1a9402a463127b2318d24c134
Josiah-tan <65082231+Josiah-tan@users.noreply.github.com>
2020-08-15 14:37:40 +1000

    Update utils.py

--- k_armed_bandits/utils.py
+++ k_armed_bandits/utils.py
@@ -8,3 +8,14 @@
 #================================================================
 
 #================================================================
+
+def sample_k_armed_bandits(cls, num_samples = 1000, plot = True):
+  """
+  sample_k_armed_bandits: samples the k_armed_bandit model with num_samples for each action and plots it using a basic boxplot
+  """
+  sample_action_vals = np.zeros((cls.num_actions, num_samples))
+  for action in cls.actions:
+    for num_sample in range(num_samples):
+      sample_action_vals[action, num_sample] = cls.get_reward(action)
+  if plot:
+    plt.boxplot(sample_action_vals.T)
