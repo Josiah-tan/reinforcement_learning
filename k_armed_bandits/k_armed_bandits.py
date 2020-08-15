22dd71fa9d2d53962afe6c9cf909b3e41ca90278
Josiah-tan <65082231+Josiah-tan@users.noreply.github.com>
2020-08-15 13:57:17 +1000

    Update k_armed_bandits.py

--- k_armed_bandits/k_armed_bandits.py
+++ k_armed_bandits/k_armed_bandits.py
@@ -15,7 +15,7 @@ class KArmedBandits:
   def __init__(self, num_actions = 10, tru_val_variance = 1, tru_val_mean = 0, sample_val_variance = 1):
     """
     __init__: initialisation of the KArmedBandits class
-      parameters -- num_actions: the number of actions that can be take
+      parameters -- num_actions: the number of actions that can be taken
                  -- tru_val_variance: variance of the true values q_star(a)
                  -- tru_val_mean: mean of the true values q_star(a)
                  -- sample_val_variance: variance of sampled rewards 
@@ -30,7 +30,7 @@ class KArmedBandits:
       """
       get_reward: gets the reward for a certain action from the normal distribution
         parameters -- action, a integer between 0 and num_actions - 1
-        returns -- action_val, a number with variance, sample_val_variance and mean, tru_action_val for a given action 
+        returns -- action_val, a number with variance-sample_val_variance and mean-tru_action_val for a given action 
       """
       tru_action_val = self.q_star[action] # selecting the tru_action_val from an index given by action
       action_val = np.random.normal(tru_action_val, self.sample_val_variance)
