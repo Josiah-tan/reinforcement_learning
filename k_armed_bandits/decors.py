5c2ae0149a42cbb51a98abdb517bc45d79e27e9e
Josiah-tan <65082231+Josiah-tan@users.noreply.github.com>
2020-08-15 13:13:01 +1000

    Update decors.py

--- k_armed_bandits/decors.py
+++ k_armed_bandits/decors.py
@@ -9,6 +9,7 @@
 
 #================================================================
 
+from joblib import Parallel, delayed
 def test_joblib_import(func):
   try: 
     Parallel(n_jobs=1)(delayed(sqrt)(i**2) for i in range(1))
