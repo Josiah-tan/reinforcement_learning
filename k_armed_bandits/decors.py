33aa469eee691dc69749c115dd4d96718ba73b5a
Josiah-tan <65082231+Josiah-tan@users.noreply.github.com>
2020-08-15 13:04:44 +1000

    Update decors.py

--- k_armed_bandits/decors.py
+++ k_armed_bandits/decors.py
@@ -8,3 +8,30 @@
 #================================================================
 
 #================================================================
+
+def test_joblib_import():
+  try: 
+    Parallel(n_jobs=1)(delayed(sqrt)(i**2) for i in range(1))
+  except:
+    from joblib import Parallel, Delayed
+
+
+class repeat:
+  def __init__(self, num_times = 10, n_jobs = 10, run_parallel = True, return_avg = True):
+    self.num_times = num_times
+    self.n_jobs = n_jobs
+    self.run_parallel = run_parallel
+    self.return_avg = return_avg
+  
+  def __call__(self, _func):
+    if self.run_parallel:
+      test_joblib_import()
+      return self.parallel_decor
+  
+  def parallel_decor(self, *args, **kwargs):
+    return_list = Parallel(n_jobs=self.n_jobs)(delayed(_func)(*args, **kwargs) for _ in range(num_times))
+    
+    if self.return_avg:
+      from numpy import mean
+      return_list = mean(return_list, axis = 0)
+    return return_list
